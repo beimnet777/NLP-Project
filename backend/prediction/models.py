0a7090e677e6b63df82255a61c9fbeae827297be
kaleabe-n <nkaleabe001@gmail.com>
2024-01-12 19:48:59 +0300

    prediction view added

--- backend/prediction/models.py
+++ backend/prediction/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+
 class Message(models.Model):
     content = models.TextField()
-    
+    prediction = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.content
--- backend/prediction/views.py
+++ backend/prediction/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render
-from rest_framework.decorators import permission_classes,api_view
+from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from .models import *
 import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from django.conf import settings
 
 # Create your views here.
+model2 = AutoModelForSequenceClassification.from_pretrained(
+    "Davlan/bert-base-multilingual-cased-finetuned-amharic", num_labels=2
+)
+
+model2.load_state_dict(torch.load(settings.BASE_DIR / "model/model.pth"))
+
+tokenizer2 = AutoTokenizer.from_pretrained(settings.BASE_DIR / "model_tokenizer")
+
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
-def predict(request:Request):
-    content = request.data.get("content")
-    message = Message.objects.create(content=content)
-    print(content)
-    res = [1]
-    if res[0]>0.5:
+def predict(request: Request):
+    text = request.data.get("content")
+    print(text)
+    Message.objects.create(content=text)
+    inputs = tokenizer2(text, return_tensors="pt")
+    outputs = model2(**inputs)
+    logits = outputs.logits
+    predicted_class = logits.argmax().item()
+    print(predicted_class, text)
+    if predicted_class == 1:
         return Response(True)
     else:
         return Response(False)
